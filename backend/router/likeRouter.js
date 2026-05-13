498dac2dd20a6784bbe252a3c086084fa9d9ff6b
pupupupupupupupupu <arsalanhassan327@gmail.com>
2026-05-13 18:10:46 +0530

    Wiping remote repository

--- backend/router/likeRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getLikeStatus, toggleLike, getUserLikes } = require("../controllers/likeController");
-const router = express.Router();
-
-router.get("/status",  getLikeStatus);   // ?userId=&comicId=
-router.post("/toggle", toggleLike);
-router.get("/:userId", getUserLikes);
-
-module.exports = router;
