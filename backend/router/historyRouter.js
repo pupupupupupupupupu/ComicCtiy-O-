ebaaf78c1c01a14c39f75b43b9212ba27f627320
pupupupupupupupupu <arsalanhassan327@gmail.com>
2026-05-13 18:27:50 +0530

    Wiping remote repository

--- backend/router/historyRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getHistory, upsertVisit } = require("../controllers/historyController");
-
-const router = express.Router();
-
-router.get("/:userId",  getHistory);
-router.post("/",        upsertVisit);
-
-module.exports = router;
--- backend/socket.js
+++
@@ -1,6 +0,0 @@
-let _io = null;
-
-const setIO = (io) => { _io = io; };
-const getIO = () => _io;
-
-module.exports = { setIO, getIO };
